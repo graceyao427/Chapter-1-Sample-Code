7f31a527d743dad5ff7eadb37757c801942970ee
Minghui Yao <130251993+graceyao427@users.noreply.github.com>
2025-08-25 14:52:38 -0700

    Update class_test.py
    update circle

--- class_test.py
+++ class_test.py
@@ -3,9 +3,12 @@ PI = 3.1415926535897 #all letters in global constant are upper-case w/ _ for spa
 #all lines must be <80 chars
 
 class Circle(): #class names should use CapWords
-    def __init__(self, radius: float):
+    def __init__(self, radius: float = 0):
         #parameter in function def should have type annotation (except self and cls)
         self.radius = radius
+
+    def __str__(self) -> str:
+        return f"Circle object with radius {self.radius}"
 
     def circumference(self) -> float: #every float should have return type annotation
         #each method should have a docstring (1 line with triple quotes) except Dunder methods
@@ -13,5 +16,13 @@ class Circle(): #class names should use CapWords
         
         Skip line to add more details.'''
         return 2 * PI * self.radius
+
+def main():
+    circle = Circle(3)
+    print(circle)
+    help(circle)
+
+if __name__ == "__main__":
+    main()
     
 
